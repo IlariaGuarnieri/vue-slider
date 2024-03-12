dd39072c695d929db7d5f279db3766f54d7df10a
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-03-12 17:38:05 +0100

    [add] autoplay

--- script.js
+++ script.js
@@ -10,14 +10,34 @@ createApp({
         'img/04.webp',
         'img/05.webp',
       ],
-      counter: 0
+      counter: 0,
       isPlayerActive: true
     }
   },
 
   methods:{
     nextPrev(isNext){
-      
+      // con operatore ternario se is next è true incremento altrimenti decremento
+      isNext ? this.counter++ : this.counter--;
+
+      // controllo validità counter
+      if(this.counter === this.images.legnth){
+        this.counter = 0
+      }else if (this.counter < 0){
+        this.counter = this.images.lenght -1;
+      }
+    },
+
+    autoplay(){
+      setInterval(() =>{
+        if(this.isPlayerActive){
+          this.nextPrev(true)
+        }
+      },1000)
     }
+  },
+
+  mounted(){
+    this.autoplay()
   }
 }).mount('#app')
